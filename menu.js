afaa21ff91e2bdf4189ad8d2f435cbefd8eb1b39
EdwingranadosF457 <edwindavidgranadosfontalvo540@gmail.com>
2025-11-15 20:48:09 -0500

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementById("menu-juegos");
-const btn = document.querySelector(".dropdown-btn");
-
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  menu.classList.toggle("open");
-});
